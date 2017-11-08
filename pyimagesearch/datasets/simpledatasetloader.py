56255ea302c7110176013a7a995ff42c24d7fd35
U G Murthy <ugmurthy@gmail.com>
2017-11-08 13:06:58 +0530

    second commit

--- pyimagesearch/datasets/simpledatasetloader.py
+++ pyimagesearch/datasets/simpledatasetloader.py
@@ -29,17 +29,16 @@ class SimpleDatasetLoader:
 			# check to see if our preprocessors are not None
 			if self.preprocessors is not None:
 				#loop over preprocessors and apply each to image
-				for p in self.preprocessors:	
+				for p in self.preprocessors:
 					image = p.preprocess(image)
 			# treat or processed image as a feature vector
 			# by updating the data list followed by the labels
 			data.append(image)
 			labels.append(label)
-			
+
 			#show an update every 'verbose' images
 			if verbose >0 and i > 0 and (i+1) % verbose == 0:
-				print ("[INFO] processed {}/{}".format(i+i,
+				print ("[INFO] processed {}/{}".format(i+1,
 					len(imagePaths)))
 		# return a tuple of data and labels
 		return (np.array(data), np.array(labels))
-	
