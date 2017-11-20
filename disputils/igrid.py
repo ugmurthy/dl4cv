ec8f1fbc447aa5390ddf0326fcff4e5e03f6fcb0
U G Murthy <ugmurthy@gmail.com>
2017-11-20 14:41:56 +0530

    Igrid with doc strings

--- disputils/igrid.py
+++ disputils/igrid.py
@@ -3,9 +3,26 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
+"""
+Modulename: igrid -> facilitaes creation and populating a grid with images
+"""
+
 class  Igrid:
-    # constructor
+    """
+    Class : Igrid
+        creates a grid of rows x cols to hold images
+        Arguments:
+            rows : int
+            cols : int
+            size : (optional) size of matplotlib figure in inches.
+                    default is 8 inches
+            cmap : (optional) colour map to use.
+                    default is "Greys" but reversed (_r)
+    """
     def __init__(self,rows,cols,size=8, cmap='Greys_r'):
+        """
+        Igrid constructor
+        """
         self.rows = rows
         self.cols = cols
         self.size = size
@@ -16,6 +33,11 @@ class  Igrid:
 
     # set image to a particular cell
     def set(self,row,col,img,draw=True):
+        """
+        draws an image at cell location given by row and col
+        if draw is False then it will just draw the grid
+        which useful while experimenting
+        """
         i = row * self.cols + col
         ax = plt.Subplot(self.fig, self.grid[i])
         if draw:
@@ -26,12 +48,21 @@ class  Igrid:
 
     # display plot on screen
     def show(self):
+        """
+        Renders the figure on screen
+        """
         #plt.subplots_adjust(left=None, right=None, top=None, bottom=None)
         plt.show()
     # close plot on screen
     def close(self):
+        """
+        close the figure
+        """
         plt.close('all')
 
     # save to file
     def save(self,fname="figure.png"):
+        """
+        Save figure to filename given by fname. Deafault name is "figure.png"
+        """
         self.fig.savefig(fname)
